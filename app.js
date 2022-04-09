9b3913daaed83e2b9cbf87a30fab420677211da8
Nari Lee <nlee64@myseneca.ca>
2022-04-09 13:12:08 -0500

    resume

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-
+// declare variables for the each radio buttons
 let hiring = document.querySelector("#contact-hiring");
 let question = document.querySelector("#contact-question");
 let comment = document.querySelector("#contact-comment");
 let div = document.querySelector("#add");
 
 hiring.addEventListener("click", function () {
+    div.innerHTML=""; // enable to add the hourly only once
     let label = document.createElement("label");
     let input = document.createElement("input");
     div.appendChild(label); // insert the label in the div of the fieldset
